61387acfb5019b0abd60bd238f6c300192a25abf
woutwoot <woutwoot13@gmail.com>
2015-01-28 23:26:24 +0100

    Initial commit

---
+++ readme.md
@@ -0,0 +1,1 @@
+Example empty project for a Bukkit plugin.
---
+++ src/main/java/com/example/example/Main.java
@@ -0,0 +1,19 @@
+package com.example.example;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * @author woutwoot
+ *         Created by on 28/01/2015 - 23:21.
+ */
+public class Main extends JavaPlugin {
+
+    public void onEnable(){
+
+    }
+
+    public void onDisable(){
+
+    }
+
+}
